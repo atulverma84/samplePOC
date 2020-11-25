baa52e7d0632c3247e8b2d6a31787807b8eef36f
Atul Verma <atul.verma@centricconsulting.com>
2020-11-25 16:55:28 +0530

    cleaned

--- api/controller.js
+++ api/controller.js
@@ -1,18 +1,12 @@
 'use strict';
 var fs = require('fs');
-// var properties = require('../package.json')
-// var distance = require('../service/distance');
 
 var sp_options = {
-    entity_id: "https://members.ifac.org/imis0/staff",
+    entity_id: "https://members.ifac.org/imis0",
     private_key: fs.readFileSync("key-file.pem").toString(),
     certificate: fs.readFileSync("cert-file.crt").toString(),
     assert_endpoint: "http://localhost:3000/",
-    // force_authn: true,
-    // auth_context: { comparison: "exact", class_refs: ["urn:oasis:names:tc:SAML:1.0:am:password"] },
-    // nameid_format: "urn:oasis:names:tc:SAML:2.0:nameid-format:transient",
-    // sign_get_request: false,
-    // allow_unencrypted_assertion: true
+
 }
 
 var login_options = {
@@ -22,34 +16,29 @@ var login_options = {
 }
 
 var saml2 = require('saml2-js');
+const { stringify } = require('querystring');
 var idp_options = {
     sso_login_url: "https://members.ifac.org/imis0/SAML/Idp.aspx",
     sso_logout_url: "https://members.ifac.org/imis0/SAML/Idp.aspx",
-    certificates: fs.readFileSync("cert-file1.crt").toString()  
+    certificates: [fs.readFileSync("cert-file.crt").toString()]  
   };
   var idp = new saml2.IdentityProvider(idp_options);
 var controllers = {
    login: function(req, res) {
-    //    var aboutInfo = {
-    //        userid: properties.name,
-    //        version: properties.version
-    //    }
-    //    res.json(aboutInfo);
-    
+           var sp = new saml2.ServiceProvider(sp_options);
+
+           var metadata = sp.create_metadata();            
           
-
-            var sp = new saml2.ServiceProvider(sp_options);
-
-            // Example use of service provider.
-            // Call metadata to get XML metatadata used in configuration.
-            var metadata = sp.create_metadata();
-            
-            console.log(idp);
-         var s =   sp.create_login_request_url(idp,sp_options, () => {
-                //console.log(s);
-                res.json({                    
-                    metadata : metadata
+             var s =   sp.create_login_request_url(idp,sp_options, (error, login_url, request_id) => {
+              
+                res.json({       
+                    error : error,
+                    login_url : login_url,
+                    request_id : request_id
                 });
+                // if (error != null)
+                //     return res.send(500);
+                // res.redirect(login_url);
             })
 
             

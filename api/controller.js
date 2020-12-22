51d792665a7cf3e5cf984d79c6088b65fba374c0
Atul Verma <atul.verma@centricconsulting.com>
2020-12-22 17:25:21 +0530

    added logout

--- api/controller.js
+++ api/controller.js
@@ -2,10 +2,10 @@
 var fs = require('fs');
 
 var sp_options = {
-    entity_id: "https://members.ifac.org/imis0",
+    entity_id: "https://ifac-digital-standards-pub.web.app/",
     private_key: fs.readFileSync("key-file.pem").toString(),
     certificate: fs.readFileSync("cert-file.crt").toString(),
-    assert_endpoint: "http://gmail.com",
+    assert_endpoint: "https://ifac-digital-standards-pub.web.app/",
 
 }
 
@@ -20,12 +20,16 @@ const { stringify } = require('querystring');
 var idp_options = {
     sso_login_url: "https://members.ifac.org/imis0/SAML/Idp.aspx",
     sso_logout_url: "https://members.ifac.org/imis0/SAML/Idp.aspx",
-    certificates: [fs.readFileSync("cert-file.crt").toString()]  
+    certificates: [fs.readFileSync("cert-file.crt").toString()],
+    force_authn: true,
+    sign_get_request: false,
+    allow_unencrypted_assertion: false  
   };
   var idp = new saml2.IdentityProvider(idp_options);
+  var sp = new saml2.ServiceProvider(sp_options);
   var controllers = {
    login: function(req, res) {
-           var sp = new saml2.ServiceProvider(sp_options);
+          
 
            var metadata = sp.create_metadata();            
           
@@ -45,12 +49,60 @@ var idp_options = {
 
             
    },
-   logout: function(req, res) {
-           distance.find(req, res, function(err, dist) {
-               if (err)
-                   res.send(err);
-               res.json(dist);
-           });
+   redirect: function(req, res) {
+  
+
+    var options = {
+        request_body : fs.readFileSync("samlResponse.xml"),
+        require_session_index : true
+    }
+   
+    sp.redirect_assert(idp, options, (error, response) => {
+
+        console.log(response);
+              
+        res.json({       
+            error : error,
+            response : response
+        });
+        // if (error != null)
+        //     return res.send(500);
+        // res.redirect(login_url);
+    })
+},
+abc: function(req, res) {
+  
+
+    var options = {
+        request_body : fs.readFileSync("samlResponse.xml")
+    }
+   
+    sp.post_assert(idp, options, (error, response) => {
+              
+        res.json({       
+            error : error,
+            response : response
+        });
+        // if (error != null)
+        //     return res.send(500);
+        // res.redirect(login_url);
+    })
+},
+logout: function(req, res) {
+    var options = {
+        name_id: "389879",
+        session_index: "session123"
+      };
+     
+      sp.create_logout_request_url(idp, options, function(err, logout_url) {
+     
+        //res.redirect(logout_url);
+        res.json({       
+            error : err,
+            logout_url : logout_url
+        });
+      });
+
        },
 };
 
